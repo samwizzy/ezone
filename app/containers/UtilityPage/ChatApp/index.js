4bc30889f9f0125723af46f8bdfcd55db4b5de7f
Teslima02 <teslimafeez@gmail.com>
2020-03-08 15:59:55 +0100

    commit ongoing chat

--- app/containers/UtilityPage/ChatApp/index.js
+++ app/containers/UtilityPage/ChatApp/index.js
@@ -1,59 +1,60 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/core/styles';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { useInjectReducer } from 'utils/injectReducer';
+import { useInjectSaga } from 'utils/injectSaga';
 import LoadingIndicator from '../../../components/LoadingIndicator';
 import * as Actions from '../actions';
 import * as Selectors from '../selectors';
-import saga from './../saga';
-import reducer from './../reducer';
-import { useInjectSaga } from 'utils/injectSaga';
-import { useInjectReducer } from 'utils/injectReducer';
-import ChatTab from './ChatTab'
-import DashboardLayout from '../components/DashboardLayout' 
+import saga from '../saga';
+import reducer from '../reducer';
+import ChatTab from './ChatTab';
+import DashboardLayout from '../components/DashboardLayout';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-  }
+  },
 }));
 
 const ChatsApp = props => {
-    useInjectReducer({ key: 'utilityPage', reducer });
-    useInjectSaga({ key: 'utilityPage', saga });
+  useInjectReducer({ key: 'utilityPage', reducer });
+  useInjectSaga({ key: 'utilityPage', saga });
 
-    const classes = useStyles();
-    const { loading, chats, match } = props;
-    const { params } = match
+  const classes = useStyles();
+  const { dispatchGetUserChats, dispatchGetAllEmployees } = props;
 
-    React.useEffect(() => {
-      // getUtilityChats()
-    }, []);
+  React.useEffect(() => {
+    dispatchGetAllEmployees();
+    dispatchGetUserChats();
+  }, []);
 
-    return (
-        <DashboardLayout>
-            <ChatTab />
-        </DashboardLayout>
-    );
+  return (
+    <DashboardLayout>
+      <ChatTab />
+    </DashboardLayout>
+  );
 };
 
 ChatsApp.propTypes = {
-  loading: PropTypes.bool,
-  chats: PropTypes.array,
-  // getUtilityChats: PropTypes.func,
+  dispatchGetAllEmployees: PropTypes.func,
+  dispatchGetUserChats: PropTypes.func,
 };
 
 const mapStateToProps = createStructuredSelector({
-    loading: Selectors.makeSelectLoading(),
-    chats: Selectors.makeSelectAllUsersChat(),
+  loading: Selectors.makeSelectLoading(),
+  chats: Selectors.makeSelectAllUsersChat(),
+  allEmployees: Selectors.makeSelectAllEmployees(),
 });
 
 function mapDispatchToProps(dispatch) {
   return {
-    // getUtilityChats: ev => dispatch(() => {}),
+    dispatchGetAllEmployees: () => dispatch(Actions.getAllUsers()),
+    dispatchGetUserChats: () => dispatch(Actions.getAllUsersChat()),
   };
 }
 
@@ -66,4 +67,5 @@ export default withRouter(
   compose(
     withConnect,
     memo,
-)(ChatsApp));
+  )(ChatsApp),
+);
