b0bbd970f8055b22dee8cc78292015db196549df
Teslim <teslimafeez@gmail.com>
2020-04-13 14:31:30 +0100

    firebase setup for push noti

--- app/services/firebaseService/firebaseService.js
+++ app/services/firebaseService/firebaseService.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import config from './firebaseServiceConfig';
 import 'firebase/auth';
 import 'firebase/database';
+import '@firebase/messaging';
 // import { AUTH_CONFIG } from '../auth0Service/auth0ServiceConfig';
 
 class FirebaseService {
@@ -20,10 +21,84 @@ class FirebaseService {
     }
     console.log('firebase come here');
     firebase.initializeApp(config);
+    const messageing = firebase.messaging();
+    messageing
+      .requestPermission()
+      .then(() => {
+        console.log('have permission');
+        return messageing.getToken();
+      })
+      .then(token => {
+        console.log(token);
+      })
+      .catch(err => {
+        console.log('error occur');
+      });
+
     this.db = firebase.database();
     this.auth = firebase.auth();
   }
 
+  createNewAdmin = data => {
+    if (!firebase.apps.length) {
+      return;
+    }
+
+    console.log(data, 'data from firebase');
+
+    const { email, password } = data;
+
+    firebase
+      .auth()
+      .fetchSignInMethodsForEmail(email)
+      .then(providers => {
+        console.log(providers.length, 'providers.length');
+        // if providers.length === 0 that mean's the user is not available
+        if (providers.length === 0) {
+          this.createCompanyNode(data);
+          this.auth
+            .createUserWithEmailAndPassword(email, password)
+            .catch(error => {
+              // Handle Errors here.
+              const errorCode = error.code;
+              const errorMessage = error.message;
+              // ...
+              console.log(errorCode, 'errorCode');
+              console.log(errorMessage, 'error.message');
+            });
+        } else {
+          // has signed up
+          console.log('nothing come heres');
+        }
+      });
+  };
+
+  createCompanyNode = data => {
+    if (!firebase.apps.length) {
+      return;
+    }
+
+    const { companyName } = data;
+    const stripSpecialCharacter = companyName.replace(
+      /[&\/\\#,+()$~%.'":*?<>{}]/g,
+      '',
+    );
+    const stripWhiteSpace = stripSpecialCharacter.trim();
+
+    console.log(stripWhiteSpace, 'stripWhiteSpace');
+
+    this.db
+      .ref()
+      .child(stripWhiteSpace)
+      .child('Users')
+      .push({
+        companyName: data.companyName,
+        email: data.email,
+        // name: `${data.firstname} ${data.lastname}`,
+        // phoneNumber: companyName,
+        timestamp: new Date().getTime(),
+      });
+  };
   //   getUserData = userId => {
   //     if (!firebase.apps.length) {
   //       return;
