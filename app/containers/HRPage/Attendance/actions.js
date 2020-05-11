3efcc51c3d69cfbecd9714996dab8bf69b7a0e6a
Samuel Okeke <samwizzy11@gmail.com>
2020-05-11 19:01:10 +0100

    Announcement dialog view fixes ongoing 8

--- app/containers/HRPage/Attendance/actions.js
+++ app/containers/HRPage/Attendance/actions.js
@@ -135,7 +135,6 @@ export function closeEditShiftDialog(data) {
 }
 
 export function openNewEmployeeShiftDialog() {
-  console.assertlog("trying employee shift dialog")
   return {
     type: Constants.OPEN_NEW_EMPLOYEE_SHIFT_DIALOG,
   };
