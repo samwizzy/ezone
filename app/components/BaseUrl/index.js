7a86fc20a436fb5620a3bf4dc7e74ca8f67236e2
Samuel Okeke <samwizzy11@gmail.com>
2020-10-02 16:14:57 +0100

    Dev baseUrl updated

--- app/components/BaseUrl/index.js
+++ app/components/BaseUrl/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // BaseUrl: ' https://dev.ezoneapps.com/gateway-prod', // development
-  BaseUrl: 'https://app.ezoneerp.com/gateway'  // production
+  BaseUrl: ' https://dev.ezoneapps.com/gateway-prod', // development
+  // BaseUrl: 'https://app.ezoneerp.com/gateway'  // production
 };
